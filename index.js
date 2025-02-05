a07244b395d7e78f9e23843248260bec5245a9fa
Emilio Neto <edentneto@gmail.com>
2025-02-05 10:42:34 -0300

    Versão Inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+var SetaEsquerda = window.document.getElementById("seta-esquerda")
+var SetaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+
+function RolarParaDireita() {
+    Leonardo.style = "display: none"
+    Bruna.style = "display: flex"
+    SetaDireita.style = "display: none"
+    SetaEsquerda.style = "display: flex; margin-top: 55px"
+}
+function RolarParaEsquerda(){
+    Leonardo.style = "display: flex"
+    Bruna.style = "display: none"
+    SetaDireita.style = "display: flex; margin-top: 55px"
+    SetaEsquerda.style = "display: none"
+}
